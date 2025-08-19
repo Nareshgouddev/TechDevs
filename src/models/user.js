7e78dee16dec8ddf9edc9d8dbe7f016016ec0785
Naresh goud <ediganareshgoud406@gmail.com>
2025-08-19 22:22:13 +0530

    Api's

--- src/models/user.js
+++ src/models/user.js
@@ -5,10 +5,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     trim: true,
+    minlength: 4,
   },
   lastName: {
     type: String,
-    required: true,
     trim: true,
   },
   email: {
@@ -22,6 +22,35 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     minlength: 6,
+  },
+  age: {
+    type: Number,
+    required: true,
+    min: 15,
+  },
+  gender: {
+    type: String,
+    required: [true, "gender is required"],
+    trim: true,
+    validate: {
+      validator: function (val) {
+        return /^(male|female|other)$/i.test(val);
+      },
+      message: (props) => `${props.value} gender not valid!`,
+    },
+  },
+  photoUrl: {
+    type: String,
+    trim: true,
+    default: "https://example.com/default-photo.jpg",
+  },
+  about: {
+    type: String,
+    default: "No information provided",
+    trim: true,
+  },
+  skills: {
+    type: [String],
   },
 });
 
