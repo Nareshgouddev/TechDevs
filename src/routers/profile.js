5d5185a6c458a14c888da6c38837d4b8f62de67c
Naresh goud <ediganareshgoud406@gmail.com>
2025-08-25 20:49:03 +0530

    validation

--- src/routers/profile.js
+++ src/routers/profile.js
@@ -3,6 +3,7 @@ const express = require("express");
 const profileRouter = express();
 
 const jwt = require("jsonwebtoken");
+const validateProfileData = require("../utils/validator");
 const { userAuth } = require("../middleware/Auth");
 
 profileRouter.post("/profile", userAuth, async (req, res) => {
@@ -12,6 +13,20 @@ profileRouter.post("/profile", userAuth, async (req, res) => {
       throw new Error("User not Exist");
     }
     res.send(user);
+  } catch (err) {
+    res.send("ERROR" + err.Message);
+  }
+});
+
+profileRouter.patch("/prifile/edit", userAuth, (req, res) => {
+  try {
+    if (!validateProfileData(req)) {
+      throw new Error("invalid Edit");
+    }
+    const loggedInuser = req.user;
+
+    Object.keys(req.body).forEach((key) => (loggedInuser[key] = req.body[key]));
+    res.send("profile updated succesfully");
   } catch (err) {
     res.send("ERROR" + err.Message);
   }
